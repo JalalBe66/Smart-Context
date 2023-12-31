d147fc25882a220c13ae492de336b1120191d999
JaBeXIX <jalalbelych011@gmail.com>
2023-12-31 20:52:52 +0100

    compliting our service1 page

--- src/Service1.js
+++ src/Service1.js
@@ -1,19 +1,42 @@
 import React from 'react'
-import GrandLogo from "./images/LogoComplet.png"
 import "./styleCSS/service1.css"
-export default function Service1() {
+import PetitLogo from "./images/logo.png"
+import GrandLogo from "./images/LogoComplet.png"
+export default function Service2() {
+  let changeColorInp=()=>{
+    let divB=document.getElementById("inp-img-serv1")
+    divB.style.border="solid 1px #FF385C"
+    divB.style.boxShadow="1px 1px 5px #FF385C"
+    divB.style.transition="0.6s"
+
+  }
+  let resetColor=()=>{
+    let divB=document.getElementById("inp-img-serv1")
+    divB.style.border="solid 1px #909090"
+    divB.style.boxShadow="none"
+
+  }
   return (
     <div>
-        <div>
-            <div>
+      <div>
+        
             <div id='grand-logo'>
-                    <img src={GrandLogo} width={"330px"} />
-                </div>
-                <div id='disc-services1'>
-                    <p>Répondez Pour Une Réponse Précise:</p>
-                </div>
+                    <img src={GrandLogo} width={"500px"} height={"500px"} />
             </div>
-        </div>
+            <div id='titre-p-serv1'>
+                <p>Réponse aux Informations Générales:</p>
+            </div>
+            <div id='contenu-p-serv1'>
+              <div id='inp-img-serv1'>
+                <input onBlur={resetColor} onFocus={changeColorInp} id='input-srv1' type="text" placeholder="Ajoutez votre projet" />
+                <img src={PetitLogo} id="logo-serv1"/>
+              </div>
+            <div>
+      
     </div>
+            </div>
+      </div>
+    </div>
+    
   )
 }
--- src/Service2.js
+++ src/Service2.js
@@ -1,27 +1,19 @@
 import React from 'react'
-import "./styleCSS/service2.css"
-import PetitLogo from "./images/logo.png"
 import GrandLogo from "./images/LogoComplet.png"
-export default function Service2() {
+import "./styleCSS/service2.css"
+export default function Service1() {
   return (
     <div>
-      <div>
-        
-            <div id='grand-logo'>
-                    <img src={GrandLogo} width={"330px"} />
-            </div>
-            <div id='disc-services2'>
-                <p>Réponse aux Informations Générales:</p>
-            </div>
-            <div id='disc-contenu-S2'>
-              <input type="text" placeholder="Ajouter Votre Projet" />
+        <div>
             <div>
-            <img src={PetitLogo} id="lg"/>
-      
-    </div>
+            <div id='grand-logo'>
+                    <img src={GrandLogo} width={"500px"} height={"500px"} />
+                </div>
+                <div id='titre-p-serv2'>
+                    <p>Répondez Pour Une Réponse Précise:</p>
+                </div>
             </div>
-      </div>
+        </div>
     </div>
-    
   )
 }
