a0a4b81a863e190f3a42966687194cb1f933dda6
JaBeXIX <jalalbelych011@gmail.com>
2024-01-02 13:34:57 +0100

    addint stayle page FAQ

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,6 @@ export default function Contact() {
   return (
     <div style={{marginTop:"20%"}}>
         <div>
-            <h1>Contact</h1>
         </div>
     </div>
   )
--- src/FAQ.js
+++ src/FAQ.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import Header from "./Component/header"
-import Footer from "./Component/footer"
+import React, { useState } from 'react';
+import "./styleCSS/FAQ.css";
 export default function FAQ() {
+  const [displayPanel1, setDisplayPanel1] = useState(false);
+  const [displayPanel2, setDisplayPanel2] = useState(false);
+
+  const togglePanel1 = () => {
+    setDisplayPanel1(!displayPanel1);
+  };
+
+  const togglePanel2 = () => {
+    setDisplayPanel2(!displayPanel2);
+  };
+
   return (
-    <div style={{marginTop:"20%"}}>
-        <div>
-            FAQ
+    <div id='FAQ-container' style={{ marginTop: "15%", marginBottom: "35%" }}>
+      <div className="wrapper">
+        <h1 style={{ textAlign: 'center', marginBottom: "13%", fontFamily: "Arial", fontSize: "40px", color: "#FF385C" }}>Frequently Asked Questions</h1>
+
+        <div className="faq">
+          <button className="accordion-serv1" onClick={togglePanel1}>
+            C'est quoi Smart Context?
+            <i></i>
+          </button>
+          <div className="panel" style={{ display: displayPanel1 ? "block" : "none", width: "70%", justifyContent: "center" }}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
+            </p>
+          </div>
         </div>
+
+        <div className="faq">
+          <button className="accordion-serv1" onClick={togglePanel2}>
+            C'est quoi Smart Context?
+            <i></i>
+          </button>
+          <div className="panel" style={{ display: displayPanel2 ? "block" : "none" , width: "70%", justifyContent: "center" }}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
