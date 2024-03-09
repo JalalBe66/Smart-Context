70b348068e5d6690024db30eb59b204da4707cda
JaBeXIX <jalalbelych011@gmail.com>
2024-03-09 01:02:53 +0100

    FAQ responsive

--- src/FAQ.js
+++ src/FAQ.js
@@ -17,45 +17,64 @@ export default function FAQ() {
   };
 
   return (
-    <div id='FAQ-container' style={{ marginTop: "15%", marginBottom: "45%" }}>
-      <div className="wrapper">
-        <h1 style={{ textAlign: 'center', marginBottom: "13%", fontFamily: "Arial", fontSize: "40px", color: "#851BFD " }}>Frequently Asked Questions</h1>
-
-        <div className="faq">
-          <button className="accordion-serv1" onClick={togglePanel1}>
-          ESL Description ?
-            <i></i>
-          </button>
-          <div className="panel" style={{ display: displayPanel1 ? "block" : "none", width: "70%", justifyContent: "center" }}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
-            </p>
-          </div>
-        </div>
-
-        <div className="faq">
-          <button className="accordion-serv1" onClick={togglePanel2}>
-          Benefits of ESL ?
-            <i></i>
-          </button>
-          <div className="panel" style={{ display: displayPanel2 ? "block" : "none" , width: "70%", justifyContent: "center" }}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
-            </p>
-          </div>
-        </div>
-        <div className="faq">
-          <button className="accordion-serv1" onClick={togglePanel3}>
-          Why use this platform ?
-            <i></i>
-          </button>
-          <div className="panel" style={{ display: displayPanel3 ? "block" : "none" , width: "70%", justifyContent: "center" }}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
-            </p>
-          </div>
-        </div>
+    
+<div className="FAQ-container" style={{marginBottom:"10%",marginTop:"10%"}}>
+  <div className="faq">
+    <button className="accordion-serv1" onClick={togglePanel1}>
+      ESL Description ?
+      <i></i>
+    </button>
+    {displayPanel1 && (
+      <div className="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus
+          nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi
+          fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget
+          varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa
+          posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+          cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
+        </p>
       </div>
-    </div>
+    )}
+  </div>
+
+  <div className="faq">
+    <button className="accordion-serv1" onClick={togglePanel2}>
+      Benefits of ESL ?
+      <i></i>
+    </button>
+    {displayPanel2 && (
+      <div className="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus
+          nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi
+          fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget
+          varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa
+          posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+          cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
+        </p>
+      </div>
+    )}
+  </div>
+
+  <div className="faq">
+    <button className="accordion-serv1" onClick={togglePanel3}>
+      Why use this platform ?
+      <i></i>
+    </button>
+    {displayPanel3 && (
+      <div className="panel">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada odio et dolor lacinia, sed finibus
+          nisl varius. Nulla facilisi. Sed vehicula risus in tortor bibendum, a faucibus velit vestibulum. Morbi
+          fringilla, quam sed venenatis finibus, eros ligula varius justo, ac lacinia ex enim vitae nisl. Proin eget
+          varius lectus. Nulla auctor lobortis metus, vitae fringilla dui dignissim at. Praesent rhoncus felis et massa
+          posuere, eu posuere lacus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+          cubilia curae; Mauris ultricies risus id semper commodo. Aliquam erat volutpat.
+        </p>
+      </div>
+    )}
+  </div>
+</div>
   );
 }
